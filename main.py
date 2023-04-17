0da6a08066c674a26bb65541ffa31618f2e18e29
Butanediol <iam@butanediol.me>
2023-04-17 22:18:25 +0800

    Custom check interval
    Also sleep random time to avoid multiple instances running at the same time

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-import mailbot
 import time
 import os
 import logging
 
 from Senders.telegram_sender import TelegramSender
 from Senders.bark_sender import BarkSender
+from mailbot import Mailbox
+from random import randint
 
 logging.basicConfig(level=os.environ.get('LOG_LEVEL', 'INFO').upper(), format='%(asctime)s \t %(levelname)s \t %(message)s')
+interval = int(os.environ.get('INTERVAL', '30'))
 
-mailbox = mailbot.Mailbox()
-
+mailbox = Mailbox()
 telegram = TelegramSender()
 bark = BarkSender()
 
+# Sleep random time to avoid multiple instances running at the same time.
+time.sleep(randint(0, interval))
+
 logging.info('Start checking...')
 while (1):
-    logging.debug('Checking...')
-    emails = mailbox.getUnseenMails()
-    if emails.__len__() != 0:
-        logging.debug('Found %d emails' % emails.__len__())
-
-    for email in emails:
+    for email in mailbox.getUnseenMails():
         telegram.send(message=email)
         bark.send(message=email)
-
-    time.sleep(30)
+    time.sleep(interval)
